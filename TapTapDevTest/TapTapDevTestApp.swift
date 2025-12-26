3771a660a59ce46842b89152c30d39c54f16c016
YeoSeongil <seongil_yeo@naver.com>
2025-12-26 15:36:35 +0900

    Initial Commit

---
+++ TapTapDevTest/TapTapDevTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapTapDevTestApp.swift
+//  TapTapDevTest
+//
+//  Created by 여성일 on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TapTapDevTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
